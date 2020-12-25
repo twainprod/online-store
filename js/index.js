6848d4bf4855a8fd7653db6da1fea86d5ff0f477
Vlad Kisel <ipon98@gmail.com>
2020-12-25 21:28:10 +0200

    removed all !important's

--- js/index.js
+++ js/index.js
@@ -3,18 +3,20 @@ $(document).ready(function () {
     function Paginate() {
         let items = $(".row .card-block");
         let numItems = items.length;
-        const perPage = 8;
+        const perPage = 8;        
         items.slice(perPage).hide();
 
         $('.pagination').pagination({
             items: numItems,
-            itemsOnPage: perPage,            
+            itemsOnPage: perPage,          
+            
             onPageClick: function (pageNumber) {
                 let showFrom = perPage * (pageNumber - 1);
                 let showTo = showFrom + perPage;
                 items.hide().slice(showFrom, showTo).show();
+                
             }
-        }); 
+        });        
     }
     // Подсчет пагинации при запуске приложения
     Paginate()
@@ -29,14 +31,15 @@ $(document).ready(function () {
             $('#searchBtn').prop('disabled', true);
         }
     });   
-
+    
     //   <!-- Remove item -->
     $('.remove').click(function () {      
         // Нужно добавить проверку на пустую страницу и переход на первую
+        // Перебрасывает на первую страницу при удалении со второй
         $(this).closest('.card-block').remove(); 
         $('.card-block').css("display", "block")       
-       
-            Paginate()  
+        
+            Paginate() 
         });  
         
                 
@@ -50,15 +53,16 @@ $(document).ready(function () {
                 let text = $(this).text().toLowerCase();
                 text.includes(value) ? card.show() : card.hide();
         });
-        // Пагинация при поиске (баг при результате > 8) 
-        $('.pagination').pagination()
-          
+        
+        // Пагинация при поиске (баг при результате > 8)         
+        $('.pagination').pagination();
     });    
     
      $('.btn-clear').click(function () {
                 $('.btn-clear').prop('disabled', true);
                 $('.card-block').show()                 
                 // Пересчет пагинации при нажатии на Clear
+                // Перебрасывает на первую страницу, а не остается на текущей
                 Paginate()
                 })               
 
